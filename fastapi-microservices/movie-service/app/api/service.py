f429983e4142f9869bf49cce925fdf4786926ae4
S.K.Venkat <sivaven@gmail.com>
2023-01-15 23:08:17 +0000

    PY_WEB: Fixed the following:  * reverted the old port value of env-var cast_service_host_url in docker-compose  * cleaned up the is_cast_present function

--- fastapi-microservices/movie-service/app/api/service.py
+++ fastapi-microservices/movie-service/app/api/service.py
@@ -7,6 +7,5 @@ CAST_SERVICE_HOST_URL = "http://localhost:8082/api/v1/casts"
 url = os.getenv("CAST_SERVICE_HOST_URL") or CAST_SERVICE_HOST_URL
 
 def is_cast_present(cast_id: int):
-    #resp = httpx.get(f'{url}/{cast_id}')
-    resp = httpx.get(f'http://cast_service:8082/api/v1/casts/{cast_id}')
-    return True if r.status_code == 200 else False
+    resp = httpx.get(f'{url}/{cast_id}')
+    return True if resp.status_code == 200 else False
