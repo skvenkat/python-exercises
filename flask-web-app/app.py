56e844e28b7f03f2d71306a3ba79cef6feb84f4e
S.K.Venkat <sivaven@gmail.com>
2023-01-28 23:56:18 +0000

    PY_WEB: Updated the main.py in flask-web-app

--- flask-web-app/app.py
+++ flask-web-app/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "Hello World!!!"
+    is_dev_env = app.config.get("DEVELOPMENT")
+    is_staging_env = app.config("DEBUG")
+    
+    if is_dev_env and is_staging_env:
+        deployment_env = "DEV Environment"
+    elif is is_staging_env:
+        deployment_env = "STAGING Environment"
+    else:
+        deployment_env = "PRODUCTION Environment"
 
+    return f"The app has been deployed in {deployment_env}!!!"
